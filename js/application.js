1e0fdf5b069361d8d1358b4e46860924e5143df5
Austin Mason <austinpmason@gmail.com>
2015-08-20 21:50:02 +0100

    updating files

--- js/application.js
+++ js/application.js
@@ -2,7 +2,7 @@ var numberOfSquares = 16;
 var width;
 
 var drawGrid = function(num) {
-	width = 650 / numberOfSquares;
+	width = 650 / num;
 	for(var i=0; i<num; i++) {
         var row = $('<div>', {id: "x" + i}).addClass('row').appendTo('#container');
 		for(var j=0; j<num; j++) {
@@ -12,14 +12,15 @@ var drawGrid = function(num) {
 	$('.square-div').css({'width': width,
 						'height': width});
 	$('.square-div').on('mouseenter', function() {
-		$(this).removeClass('background').addClass('background2');
+		$(this).css('opacity', '-=0.2');
+		/*$(this).removeClass('background').addClass('background2');*/
 	});
 }
 
 $(document).ready(function() {
 	drawGrid(numberOfSquares);
 	$('#clear-button').on('click', function() {
-		$('.square-div').removeClass('background2').addClass('background');
+		/*$('.square-div').removeClass('background2').addClass('background');*/
 		numberOfSquares = prompt("Enter a number of squares for each row");
 		$('#container').empty();
 		drawGrid(numberOfSquares);
