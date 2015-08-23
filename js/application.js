d32f5cfcac4df89a023425136bcceba188afa23e
Austin Mason <austinpmason@gmail.com>
2015-08-23 19:31:10 +0100

    Updating files

--- js/application.js
+++ js/application.js
@@ -1,28 +1,111 @@
-var numberOfSquares = 16;
-var width;
-
 var drawGrid = function(num) {
-	width = 650 / num;
+	var width = 650 / num;
+	var text="";
 	for(var i=0; i<num; i++) {
-        var row = $('<div>', {id: "x" + i}).addClass('row').appendTo('#container');
 		for(var j=0; j<num; j++) {
-			$('<div>', {class: "square-div background"}).appendTo(row);
+			text += '<div class="div1"><div class="square-div"></div></div>';
 		}
 	}
+	$('#container').append(text);
 	$('.square-div').css({'width': width,
 						'height': width});
+}
+
+var normal = function() {
+	$('.square-div').on('mouseenter', function(){
+		$(this).css({"background": "#FFFF66"});
+	});
+}
+
+var randomColour = function() {
+	$('.square-div').on('mouseenter', function() {
+		var num1 = Math.floor((Math.random() * 255) + 1);
+		var num2 = Math.floor((Math.random() * 255) + 1);
+		var num3 = Math.floor((Math.random() * 255) + 1);
+		$(this).css({"background": "rgb(" +num1 + ", " + num2 + ", " + num3 + ")"})
+	});
+}
+
+var trail = function() {
+	$('.square-div').on('mouseenter', function() {
+		$(this).fadeTo(0,0).fadeIn(600,1);
+	}).on('mouseleave', function () {
+		$(this).fadeTo(600,1);
+	});
+}
+
+var mouseClick = function() {
+	isMouseDown = false;
+
+    $('body').mousedown(function() {
+        isMouseDown = true;
+    }).mouseup(function() {
+        isMouseDown = false;
+    })
+
+    $('#container').on('mouseover', 'div', function() {
+        if(isMouseDown)
+            $(this).css({backgroundColor:'orange'});
+    });
+}
+
+var opacity = function() {
 	$('.square-div').on('mouseenter', function() {
 		$(this).css('opacity', '-=0.2');
-		/*$(this).removeClass('background').addClass('background2');*/
 	});
 }
 
 $(document).ready(function() {
-	drawGrid(numberOfSquares);
-	$('#clear-button').on('click', function() {
-		/*$('.square-div').removeClass('background2').addClass('background');*/
-		numberOfSquares = prompt("Enter a number of squares for each row");
-		$('#container').empty();
-		drawGrid(numberOfSquares);
+	var userInput;
+	if(userInput === undefined) {
+		userInput = 16;
+	}
+	drawGrid(16);
+	$('#row-input').on('keypress', function(e) {
+		if(e.keyCode == 13) {
+			userInput = $(this).val();
+			if(userInput !== "" && userInput > 0 && userInput < 121) {
+				$('#container').empty();
+				drawGrid(userInput);
+				$('#row-input').val("");
+			} else {
+				alert("Please enter a value between 1 and 120");
+				$('#container').empty();
+				drawGrid(16);
+				$('#row-input').val("");
+			}
+		}
 	});
+	$("input").click(function(e){
+	    var idClicked = e.target.id;
+	    switch(idClicked) {
+	    	case "normal-button":
+	    		$('#container').empty();
+				drawGrid(userInput);
+	    		normal();
+	    		break;
+	    	case "random-button":
+	    		$('#container').empty();
+				drawGrid(userInput);
+	    		randomColour();
+	    		break;
+	    	case "trail-button":
+	    		$('#container').empty();
+				drawGrid(userInput);
+	    		trail();
+	    		break;
+	    	case "mouse-button":
+	    		$('#container').empty();
+				drawGrid(userInput);
+	    		mouseClick();
+	    		break;
+	    	case "opacity-button":
+	    		$('#container').empty();
+				drawGrid(userInput);
+	    		opacity();
+	    	case "grid-button":
+	    		$('.div1').toggleClass('grid');
+	    }
+	});
+	
 });
